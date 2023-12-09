1f98a6d0f5ef8502d89f2089fc0bc5ffcd0f8ec0
AIsBreaker <gh1@aisbreaker.org>
2023-12-09 18:12:15 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,26 @@
+# Simple example for AIsBreaker with JavaScript and NodeJS: Simple Chat.
+
+## Introduction
+
+This repo contains the demo applications `aisbreaker-simple-chat.js`
+to demonstrate the usage of `aisbreaker-api-js` in a JavaScript NodeJS application.
+
+AIsBreaker offers a developer-friendly API for streamlined access to a variety of generative AI services, including OpenAI/ChatGPT, all Hugging Face AIs, and more ... to provide simplicity and to avoid vendor lock-in.
+
+Docs: [aisbreaker.org](https://aisbreaker.org/)
+
+## Run the demo
+Steps to run:
+
+    # install dependencies
+    npm install
+
+    # make changes in the .js file like changing serviceId or adding an API key (optional)
+
+    # run
+    npm start
+
+
+## Source Code
+* GitHub repo: [aisbreaker-example-simple-chat-node-js](https://github.com/aisbreaker/aisbreaker-example-simple-chat-node-js)
+* run and edit in a browser IDE: [aisbreaker-example-simple-chat-node-js in StackBlitz](https://stackblitz.com/github/aisbreaker/aisbreaker-example-simple-chat-node-js?title=AIsBreaker+Example+Simple+Chat-node-js&file=aisbreaker-simple-chat.js&startScript=npm+install+%26%26+npm+start)
---
+++ aisbreaker-simple-chat.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+//
+// Simple example for AIsBreaker with JavaScript and NodeJS: simple chat.
+//
+// Run it with:
+//   node aisbreaker-simple-chat.js
+//
+
+import { api, init } from "aisbreaker-api-js";
+
+
+console.log("aisbreaker-example-simple-chat");
+console.log("------------------------------");
+
+// service initialization
+const aisbreakerServerURL = "https://api.demo.aisbreaker.org/";
+const servicePros = {
+    //serviceId: "chat:dummy",
+    serviceId: "chat:openai.com",
+}
+const auth = {
+    secret: process.env.OPENAI_API_KEY || "",
+}
+//init();
+const aisService = api.AIsBreaker.getInstance().
+    getRemoteAIsService(aisbreakerServerURL, servicePros, auth);
+
+
+
+    // 1st question/prompt
+const question1 = "What is NodeJS?";
+console.log(`***** Question1 *****\n${question1}\n`);
+
+// 1st answer
+const response1 = await aisService.process({
+    inputs: [ {
+        text: {
+            role: "user",
+            content: question1,
+        },
+    } ],
+});
+console.log(`***** Answer1 *****\n${response1.outputs[0].text.content}\n`);
+
+
+
+// 2nd question/prompt
+const question2 = "Please translate to German";
+console.log(`***** Question2 *****\n${question2}\n`);
+
+// 2nd answer
+const response2 = await aisService.process({
+    inputs: [ {
+        text: {
+            role: "user",
+            content: question2,
+        },
+    } ],
+    conversationState: response1.conversationState,
+})
+console.log(`***** Answer2 *****\n${response2.outputs[0].text.content}\n`);
